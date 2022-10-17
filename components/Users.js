70ff269f5ef6441b1e79e3aca90cafb06a856855
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-10-16 21:59:50 -0400

    Update Users.js
    rewrite function component with a class

--- components/Users.js
+++ components/Users.js
@@ -1,16 +1,53 @@
-import { useState } from 'react';
-import User from './User';
+import { Component, useState } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+class Users extends Component {
+  constructor() {
+    super();
+    const DUMMY_USERS = [
+      { id: "u1", name: "Max" },
+      { id: "u2", name: "Manuel" },
+      { id: "u3", name: "Julie" },
+    ];
+    this.state = {
+      showUsers: true,
+      curUser: DUMMY_USERS,
+    };
+  }
+  toggleUsersHandler() {
+    this.setState(() => {
+      return { showUsers: !this.state.showUsers };
+    });
+  }
+  render() {
+    const usersList = (
+      <ul>
+        {this.state.curUser.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
-const Users = () => {
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+const Users1 = () => {
   const [showUsers, setShowUsers] = useState(true);
+  const DUMMY_USERS = [
+    { id: "u1", name: "Max" },
+    { id: "u2", name: "Manuel" },
+    { id: "u3", name: "Julie" },
+  ];
 
   const toggleUsersHandler = () => {
     setShowUsers((curState) => !curState);
@@ -27,7 +64,7 @@ const Users = () => {
   return (
     <div className={classes.users}>
       <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
+        {showUsers ? "Hide" : "Show"} Users
       </button>
       {showUsers && usersList}
     </div>
