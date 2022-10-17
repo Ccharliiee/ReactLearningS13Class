8dc25dcc51a9034e025ce2a15a2fbbb6c4a2fafb
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2022-10-16 22:50:16 -0400

    Update UserFinder.js
    Rewrite filter component with a reactive class and make a dummy API request

--- components/UserFinder.js
+++ components/UserFinder.js
@@ -1,20 +1,59 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Component } from "react";
 import classes from "./UserFinder.module.css";
 
 import Users from "./Users";
-const DUMMY_USERS = [
+const API_DUMMY_USERS = [
   { id: "u1", name: "Max" },
   { id: "u2", name: "Manuel" },
   { id: "u3", name: "Julie" },
 ];
 
-const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+class UserFinder extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usersTable: [],
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+  componentDidMount() {
+    this.setState({ usersTable: API_DUMMY_USERS });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.state.usersTable.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+
+        <Users users={this.state.filteredUsers} />
+      </>
+    );
+  }
+}
+
+const UserFinder1 = () => {
+  const [filteredUsers, setFilteredUsers] = useState(API_DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     setFilteredUsers(
-      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+      API_DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
     );
   }, [searchTerm]);
 
